128432f084db656c3e3f69981b22a8a03e0b29ba
Yu MUROTANI <y_murotani@yumemi.co.jp>
2023-12-28 22:27:46 +0900

    feat: linterを導入

--- app/src/health.ts
+++ app/src/health.ts
@@ -1,9 +1,14 @@
 import { Context } from 'hono';
 
+/**
+ * healthCheckは、サーバーの健康状態を確認するための関数です。
+ * @param {Context} c - HonoのContextオブジェクト
+ * @return {Object} 健康状態を示すJSONレスポンス
+ */
 function healthCheck(c: Context) {
-	return c.json({
-		message: 'Health check OK',
-	});
+    return c.json({
+        message: 'Health check OK',
+    });
 }
 
 export { healthCheck };
